9184887e725e430a654aa0533e0b1d0293432b7b
Dylan LaRue <dylan@Dylans-iMac.askey.com>
2022-12-21 19:44:17 -0800

    Added new questions and squence of questins and fix 48 week error

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    let numberAnwser = parseFloat(stringAnswer)
+    console.log("numberAnswer", numberAnwser, typeof numberAnwser)
+    weeklyExpenses = weeklyExpenses + numberAnwser
+    console.log("weeklyExpenses", weeklyExpenses)
+}
+for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numberAnwser = parseFloat(stringAnswer)
+    console.log("numberAnswer", numberAnwser, typeof numberAnwser)
+    monthlyExpenses = monthlyExpenses + numberAnwser
+    console.log("monthlyExpenses", monthlyExpenses)
+}
+for (let i = 0; i < totalAnnualExpenses.length; i++) {
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    let numberAnwser = parseFloat(stringAnswer)
+    console.log("numberAnswer", numberAnwser, typeof numberAnwser)
+    totalAnnualExpenses = annualExpenses + numberAnwser
+    console.log("totalAnnualExpenses", totalAnnualExpenses)
+}
